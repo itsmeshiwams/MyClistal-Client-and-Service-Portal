d84a8fe666ab110cbc1fde9e67b57e320c431ef8
Suprim Bhattarai <supremebhattarai497@gmail.com>
2025-11-07 18:39:26 +0545

    improvement in Notfound Refresh and Tasks

--- Frontend/myclistal/src/pages/NotFound.jsx
+++ Frontend/myclistal/src/pages/NotFound.jsx
@@ -1,10 +1,32 @@
 // src/pages/NotFound.jsx
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
 
 export default function NotFound() {
+  const location = useLocation();
+
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold text-red-600">404 | Page Not Found</h1>
+    <div className="flex">
+      <div className="h-screen sticky top-0">
+        <Sidebar />
+      </div>
+      <div className=" flex-1 flex flex-col">
+        <Navbar />
+        <div className="flex text-center justify-center flex-col items-center h-full p-6">
+          <h1 className="text-3xl font-bold text-red-600 mb-2">
+            404 | Page{" "}
+            <span className="font-mono text-blue-600">{location.pathname}</span>{" "}
+            Not Found
+          </h1>
+          <p className="text-lg text-gray-700">
+            The page{" "}
+            <span className="font-mono text-blue-600">{location.pathname}</span>{" "}
+            does not exist.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
