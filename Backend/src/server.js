bfc7d4e0851535ae647188dfdf7ba09f161ee894
Shiwams Phuyal <itsmeshiwams@gmail.com>
2026-01-04 08:07:18 +0545

    Server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -23,7 +23,7 @@ connectDB()
     const io = new Server(server, {
       cors: {
         origin: process.env.FRONTEND_URL || "http://localhost:5173",
-        methods: ["GET", "POST"],
+        methods: ["GET", "POST","PUT", "PATCH","DELETE"],
         credentials: true,
       },
     });
